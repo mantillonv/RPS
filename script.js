56a8f14b950e9c37268b6d905a4670d782e397a4
mantillonv <marceloantillon93@gmail.com>
2023-10-12 12:41:38 -0700

    Add functions, loop

--- script.js
+++ script.js
@@ -1,11 +1,54 @@
-/* Rock Papers Scissors */
-function getComputerChoice() {
-    const randomNumber = Math.random();
-    if (randomNumber < 0.33) {
-        return "Rock";
-    } else if (randomNumber < 0.66) {
-        return "Paper";
+function getUserChoice() {
+    let userInput = prompt("Rock, Paper or Scissors? Your Move!").toLowerCase();
+    if (userInput === "rock" || userInput === "paper" || userInput === "scissors") {
+        return userInput;
     } else {
-        return "Scissors";
+        console.log("Invalid choice. Please choose Rock, Paper, or Scissors.");
+        return getUserChoice();
     }
 }
+
+function getComputerChoice() {
+    const randomInt = Math.random();
+    if (randomInt < 0.33) {
+        return "rock";
+    } else if (randomInt < 0.66) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return "It's a tie"; 
+    } else if ((playerSelection === "rock" && computerSelection === "scissors") ||
+               (playerSelection === "paper" && computerSelection === "rock") ||
+               (playerSelection === "scissors" && computerSelection === "paper")
+    ) {
+        return "You win";
+    } else {
+        return "Computer wins";
+    }
+}
+
+let userScore = 0;
+let computerScore = 0;
+
+function playGame() {
+    for (let i = 0; i < roundsInt; i++) {
+        const playerSelection = getUserChoice();
+        const computerSelection = getComputerChoice();
+        const result = playRound (playerSelection, computerSelection);
+    console.log(result);
+    if (result === "You win") {
+        userScore++;
+    } else if (result === "Computer wins") {
+        computerScore++;
+    }
+    console.log("Final Score - You: " + userScore + " Computer: " + computerScore);
+    }
+}
+
+const roundsInt = 5;
+playGame(roundsInt);
