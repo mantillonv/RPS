8e63f3fba19f02ca03ea8cea740d014f356f267f
mantillonv <marceloantillon93@gmail.com>
2023-10-20 14:08:44 -0700

    Last rps changes v1.0

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-// Audio //
+// Audio
 const audio = document.getElementById("soundtrack");
-audio.volume = 0.4;
+audio.volume = 0.45;
 
 const hoverSound = document.getElementById("hoverSound");
+const selectSound = new Audio("./sfx/select.mp3");
+const victorySound = new Audio("./sfx/victory.mp3");
 
 const rockImgA = document.querySelector(".gameBoard .playerSection .playerImg #rock");
 const paperImgA = document.querySelector(".gameBoard .playerSection .playerImg #paper");
@@ -17,11 +19,17 @@ function playHoverSound() {
     hoverSound.play();
 }
 
-// Game Logic //
+// Game Logic
 const roundsInt = 5;
 let userScore = 0;
 let computerScore = 0;
 let tieScore = 0;
+let isRoundInProgress = false;
+
+function displayWinnerMessage(winner) {
+    const winnerDiv = document.querySelector(".winnerDiv");
+    winnerDiv.textContent = `Winner: ${winner}`;
+}
 
 function getUserChoice(choice) {
     return choice;
@@ -55,9 +63,11 @@ function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
         result = "It's a tie";
         tieScore++;
-    } else if ((playerSelection === "rock" && computerSelection === "scissors") ||
-               (playerSelection === "paper" && computerSelection === "rock") ||
-               (playerSelection === "scissors" && computerSelection === "paper")) {
+    } else if (
+        (playerSelection === "rock" && computerSelection === "scissors") ||
+        (playerSelection === "paper" && computerSelection === "rock") ||
+        (playerSelection === "scissors" && computerSelection === "paper")
+    ) {
         result = "You win";
         userScore++;
     } else {
@@ -70,7 +80,7 @@ function playRound(playerSelection, computerSelection) {
     resultsElement.textContent = result;
 
     setTimeout(() => {
-        resultsElement.textContent = ""; 
+        resultsElement.textContent = "";
     }, 2000);
 }
 
@@ -88,17 +98,16 @@ function updateScoreDisplay() {
     tieScoreElement.textContent = "Ties: " + tieScore;
 
     if (userScore >= roundsInt) {
-    } else if(computerScore >= roundsInt) {
+    } else if (computerScore >= roundsInt) {
     }
 }
 
-
-
 function playGame(playerChoice) {
-    if (userScore >= roundsInt - 2 || computerScore >= roundsInt - 2) {
-        endGame();
+    if (isRoundInProgress) {
         return;
     }
+
+    isRoundInProgress = true;
 
     const playerChoiceElements = document.querySelectorAll('.playerChoice img');
     const computerChoiceElements = document.querySelectorAll('.computerChoice img');
@@ -117,6 +126,9 @@ function playGame(playerChoice) {
     }
 
     playerChoiceElement.style.display = 'block';
+
+    selectSound.currentTime = 0;
+    selectSound.play();
 
     startTimer(() => {
         const computerChoice = getComputerChoice();
@@ -137,32 +149,46 @@ function playGame(playerChoice) {
         resultSound.play();
 
         setTimeout(() => {
-            playerChoiceElements.forEach(img => img.style.display = 'none'); 
+            playerChoiceElements.forEach(img => img.style.display = 'none');
             computerChoiceElements.forEach(img => img.style.display = 'none');
+
+            isRoundInProgress = false;
+
+            if (userScore >= roundsInt - 2 || computerScore >= roundsInt - 2) {
+                endGame();
+            }
         }, 1000);
     });
 }
 
-document.getElementById("rock").addEventListener("click", function() {
+document.getElementById("rock").addEventListener("click", function () {
     playGame("rock");
 });
 
-document.getElementById("paper").addEventListener("click", function() {
+document.getElementById("paper").addEventListener("click", function () {
     playGame("paper");
 });
 
-document.getElementById("scissors").addEventListener("click", function() {
+document.getElementById("scissors").addEventListener("click", function () {
     playGame("scissors");
 });
 
 function endGame() {
     const winnerDiv = document.querySelector(".winnerDiv");
     const winnerText = userScore > computerScore ? "You win!" : "Computer wins!";
-    winnerDiv.innerHTML = `<h2>${winnerText}</h2><button class="reset">Play Again?</button>`
-    const winSound = new Audio("./sfx/result.wav");
-    winSound.play();
-    const resetButton = document.querySelector(".reset");
-    resetButton.addEventListener("click", resetGame);
+    winnerDiv.innerHTML = `<h2>${winnerText}</h2>`;
+
+    victorySound.currentTime = 0;
+    victorySound.play();
+
+    const modal = document.getElementById("myModal");
+    modal.style.display = "block";
+
+    const playAgainBtn = document.getElementById("playAgainBtn");
+    playAgainBtn.addEventListener("click", function () {
+        modal.style.display = "none";
+        resetGame();
+    });
 }
 
 function startTimer(callback) {
@@ -176,4 +202,33 @@ function startTimer(callback) {
             callback();
         }
     }, 1000);
+}
+
+function resetGame() {
+    userScore = 0;
+    computerScore = 0;
+    tieScore = 0;
+    isRoundInProgress = false;
+
+    const playerChoiceElements = document.querySelectorAll('.playerChoice img');
+    const computerChoiceElements = document.querySelectorAll('.computerChoice img');
+    playerChoiceElements.forEach(img => img.style.display = 'none');
+    computerChoiceElements.forEach(img => img.style.display = 'none');
+
+    const winnerDiv = document.querySelector(".winnerDiv");
+    winnerDiv.innerHTML = "";
+
+    const rockButton = document.getElementById("rock");
+    const paperButton = document.getElementById("paper");
+    const scissorsButton = document.getElementById("scissors");
+
+    rockButton.removeEventListener("click", () => playGame("rock"));
+    paperButton.removeEventListener("click", () => playGame("paper"));
+    scissorsButton.removeEventListener("click", () => playGame("scissors"));
+
+    rockButton.addEventListener("click", () => playGame("rock"));
+    paperButton.addEventListener("click", () => playGame("paper"));
+    scissorsButton.addEventListener("click", () => playGame("scissors"));
+
+    updateScoreDisplay();
 }
